67ce1e0bf6b0dcf75371ef7f44a2537f83eb1473
pritika mishra <pritikamishra2017@gmail.com>
2023-06-07 20:11:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css"
+import React, { useState } from 'react';
+
+const CounterApp = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  const decrement = () => {
+    setCount(prevCount => prevCount - 1);
+  };
+
+  return (
+    <div className="App">
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default CounterApp;
